1ebb7d549165ba56139737153ecf8366fac68656
chenshenhai <cshenhai@gmail.com>
2021-03-21 13:01:11 +0800

    chore: add e2e testing

---
+++ scripts/screen.config.js
@@ -0,0 +1,8 @@
+const pageList = [
+  { path: 'idraw/examples/path/draw.html', w: 600, h: 600, delay: 200 },
+  { path: 'idraw/examples/path/play.html', w: 600, h: 600, delay: 8000 },
+]
+
+module.exports = {
+  pageList,
+}
