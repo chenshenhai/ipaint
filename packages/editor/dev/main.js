b128a4b6783828587f40ad84a351f41e6dac9979
chenshenhai <cshenhai@gmail.com>
2022-04-27 23:20:08 +0800

    script: init vite dev mode

---
+++ packages/editor/dev/main.js
@@ -0,0 +1,30 @@
+import iPaintEditor from '../src/index'
+
+async function main() {
+  const dom = document.querySelector('#ipaint');
+  const ipaint = new iPaintEditor(dom, {
+    width: window.innerWidth,
+    height: window.innerHeight,
+
+    // 72px
+    // canvasWidth: 595,
+    // canvasHeight: 842,
+
+    // 150px
+    canvasWidth: 1240,
+    canvasHeight: 1754,
+
+    // 300 px
+    // canvasWidth: 2479,
+    // canvasHeight: 3508,
+  });
+  
+  ipaint.ready().then(() => {
+    console.log('paint ready!');
+    // paint.setFacsimileImage('./images/copy.png');
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+main();
